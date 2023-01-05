de66a6b332846d278ad9f061432b82d1127001d8
grandiloquent <1785907865@qq.com>
2023-01-05 14:19:17 +0800

    更新翻译功能

--- translateName.js
+++ translateName.js
@@ -1,14 +1,13 @@
 const vscode = require('vscode');
-const fs = require('fs');
-const path = require('path'); 
+const {replaceSelection, camel, translate} = require("./utils");
 
 const COMMAND = 'knife.translateName';
- 
+
 module.exports = (context) => {
     context.subscriptions.push(vscode.commands.registerCommand(COMMAND, async () => {
-        const fileName = vscode.window.activeTextEditor.document.fileName;
-        const strings = await vscode.env.clipboard.readText();
-       
+        replaceSelection(async (text) => {
+            return `${camel(await translate(text))}`;
+        })
     }));
 }
 /*
