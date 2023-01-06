5cbaad50e23c39ea6c34f398762e488ed17f48e3
grandiloquent <1785907865@qq.com>
2023-01-06 15:30:33 +0800

    更新

--- formatInlineStyle.js
+++ formatInlineStyle.js
@@ -1,14 +1,50 @@
 const vscode = require('vscode');
 const fs = require('fs');
-const path = require('path'); 
+const path = require('path');
 
 const COMMAND = 'knife.formatInlineStyle';
- 
+
+function extractStyle() {
+    const editor = vscode.window.activeTextEditor;
+    const selection = editor.selection;
+    const strings = editor.document.getText(selection);
+    const match = /(<*?[a-zA-Z0-9_-]+) (style="([^"]+)")/.exec(strings);
+    let name = match[1];
+    let body = match[3];
+    if (name.startsWith("<")) {
+        name = name.slice(1);
+    } else {
+        name = '.' + name;
+    }
+    return {name, body, match};
+}
+
+function formatInSameHtmlPage() {
+
+
+    let text = editor.document.getText();
+    const obj = extractStyle();
+    if (obj.name.startsWith(".")) {
+        text = text.replace('</style>', `${obj.name}{
+${obj.body}
+}`).replace(obj.match[0], `class="${obj.name.slice(1)}"`)
+            .replaceAll(obj.match[2], `class="${obj.name.slice(1)}"`)
+    } else {
+        text = text.replace('</style>', `${obj.name}{
+${obj.body}
+}`).replaceAll(obj.match[2], '')
+
+    }
+    let invalidRange = new vscode.Range(0, 0, editor.document.lineCount, 0);
+    let validFullRange = editor.document.validateRange(invalidRange);
+    editor.edit(
+        edit =>
+            edit.replace(validFullRange, text));
+}
+
 module.exports = (context) => {
     context.subscriptions.push(vscode.commands.registerCommand(COMMAND, async () => {
-        const fileName = vscode.window.activeTextEditor.document.fileName;
-        const strings = await vscode.env.clipboard.readText();
-       
+        formatInSameHtmlPage();
     }));
 }
 /*
