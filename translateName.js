5ae0fd6fddf3eae0472b4e7aee348c0a4d1efc90
grandiloquent <1785907865@qq.com>
2023-01-06 10:34:13 +0800

    更新翻译功能

--- translateName.js
+++ translateName.js
@@ -5,9 +5,13 @@ const COMMAND = 'knife.translateName';
 
 module.exports = (context) => {
     context.subscriptions.push(vscode.commands.registerCommand(COMMAND, async () => {
-        replaceSelection(async (text) => {
-            return `${camel(await translate(text))}`;
+        const activeEditor = vscode.window.activeTextEditor;
+        const {text, range} = activeEditor.document.lineAt(activeEditor.selection.active.line);
+        const strings = await translate(text.trim());
+        activeEditor.edit(editBuilder => {
+            editBuilder.replace(range, `${camel(strings)}`)
         })
+
     }));
 }
 /*
