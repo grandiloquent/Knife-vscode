01ee9063791a956376f0443982e43cb7cd47b923
grandiloquent <1785907865@qq.com>
2023-01-31 03:53:48 +0800

    更新

--- createWebComponents.js
+++ createWebComponents.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const {upperCamel} = require("./utils");
 
-const COMMAND = 'knife.createLit';
+const COMMAND = 'knife.createWebComponents';
 
 module.exports = (context) => {
     context.subscriptions.push(vscode.commands.registerCommand(COMMAND, async () => {
         // `C:\\Users\\Administrator\\WeChatProjects\\yg\\app\\components`;
 
-        const dir = `C:\\Users\\Administrator\\Desktop\\Resources\\SourceCode\\Video\\app\\src\\main\\jni\\static`;
+        const dir = `C:\\Users\\Administrator\\WeChatProjects\\yg\\app\\backend`;
         const strings = (await vscode.env.clipboard.readText()).trim();
         const fileName = path.join(dir, `custom-${strings}.js`);
         console.log(fileName)
@@ -18,18 +18,15 @@ module.exports = (context) => {
             return
         }
         const name=upperCamel(strings);
-        fs.writeFileSync(fileName, `export class Custom${name} extends HTMLElement {
+        fs.writeFileSync(fileName, `(()=>{class Custom${name} extends HTMLElement {
 
   constructor() {
     super();
     this.attachShadow({
       mode: 'open'
     });
-    const wrapper = document.createElement("div");
-    wrapper.setAttribute("class", "wrapper");
     const style = document.createElement('style');
     style.textContent = \` \`;
-    this.wrapper = wrapper;
     this.shadowRoot.append(style, wrapper);
   }
   navigate(e) {
@@ -38,16 +35,7 @@ module.exports = (context) => {
     }));
   }
   set data(value) {
-    this.wrapper.insertAdjacentHTML('afterbegin',value.map(element => {
-      return \`<div bind class="item"  data-href="${element.href}" @click="navigate">
-      <svg viewBox="0 0 24 24" style="width:24px;height:24px;">
-      ${element.path}
-      </svg>
-      <div class="item-title">
-      ${element.title}
-        </div>
-        </div>\`
-    }).join(''));
+     
     this.wrapper.querySelectorAll('[bind]').forEach(element => {
       if (element.getAttribute('bind')) {
         this[element.getAttribute('bind')] = element;
@@ -73,12 +61,13 @@ customElements.define('custom-${strings}', Custom${name});
 /*
 <!--
 <script type="module" src="./components/custom-${strings}.js"></script>
-<custom-${strings}></custom-g>
+<custom-${strings} bind="customm${name}" @submit="onCustomm${name}Submit"></custom-${strings}>
 customElements.whenDefined('custom-${strings}').then(() => {
   customm${name}.data = []
 })
 -->
-*/`);
+*/
+})();`);
 
     }));
 }
